fa87ea28cad5143b86ea9549dc30b82ebf05f99c
simransin <simransingh9876rajgir@gmail.com>
2023-08-10 00:31:12 +0530

    24th

--- Form_handler.php
+++ Form_handler.php
@@ -1,23 +1,23 @@
 <?php
 $name = $_POST['name'];
-$visitor = $_POST['email'];
+$visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
 $email_from = 'info@mywebsite.com';
 $email_subject = "New From Submission";
 
-$email_body = "User Name: $name.\n"
-                "User Email: $visitor.\n"
-                "Subject: $subject.\n"
-                "User Message: $message.\n"
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message.\n".
 
 
 $to = 'simransingh9876rajgir@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
-$headers = "Reply-To: $visitor_email \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to, $email_subject,$email_body,$headers);
 
